e7a24f5378c634979e0d0418e7cc674e5198c158
Amit P Naik <amit.naik8103@gmail.com>
2020-06-01 19:04:51 +0530

    Added test case

---
+++ CustomerEFCore/CustomerEFCore.Test/RepositoryTest.cs
@@ -0,0 +1,55 @@
+﻿using CustomerEFCore.Data;
+using CustomerEFCore.Domain;
+using CustomerEFCore.Repository;
+using CustomerEFCore.Repository.Contract;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+
+namespace CustomerEFCore.Test
+{
+    public class RepositoryTest
+    {
+        private Mock<ICustomerRepository> customerRepositoryMock;
+        private DbContextOptionsBuilder builder;
+        private Mock<Customer> customerMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            customerRepositoryMock = new Mock<ICustomerRepository>();
+
+
+
+            builder = new DbContextOptionsBuilder();
+            builder.UseInMemoryDatabase("InMemoryCustomerDB");
+
+            customerMock = new Mock<Customer>();
+
+
+
+        }
+
+
+        [Test]
+        public void CheckCustomerRepositoryAddCustomer()
+        {
+
+            using (var context = new CustomerContext(builder.Options))
+            {
+                Customer customer = new Customer
+                {
+                    CustomerName = "Shweta Naik",
+                    Address = "Bangalore"
+                };
+                var customerRepository = new CustomerRepository(context);
+                customerRepository.AddCustomer(customer);
+                var result = context.SaveChanges();
+                Assert.AreEqual(1, result);
+
+            }
+
+
+        }
+    }
+}
