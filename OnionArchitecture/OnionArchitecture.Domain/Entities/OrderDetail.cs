d4c5331bfa828d1b789dfb6f52d0812a1d893eff
Amit P Naik <amit.naik8103@gmail.com>
2020-06-15 13:03:01 +0530

    Added ef migration

--- OnionArchitecture/OnionArchitecture.Domain/Entities/OrderDetail.cs
+++ OnionArchitecture/OnionArchitecture.Domain/Entities/OrderDetail.cs
@@ -1,7 +1,9 @@
 ﻿namespace OnionArchitecture.Domain.Entities
 {
-    public class OrderDetail : BaseEntity
+    public class OrderDetail  
     {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
         public Order Orders { get; set; }
         public Product Product { get; set; }
     }
