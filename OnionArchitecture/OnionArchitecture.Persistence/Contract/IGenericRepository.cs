9b14a3680e17977a927fd2198b828ae8d64b93e1
Amit P Naik <amit.naik8103@gmail.com>
2020-06-17 12:33:13 +0530

    added unit testing

---
+++ OnionArchitecture/OnionArchitecture.Persistence/Contract/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace OnionArchitecture.Persistence.Contract
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T obj);
+        void Update(T obj);
+        void Delete(object id);
+        void Save();
+    }
+}
---
+++ OnionArchitecture/OnionArchitecture.Persistence/Repository/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnionArchitecture.Data;
+using OnionArchitecture.Persistence.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Persistence.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private DbSet<T> table = null;
+
+        private readonly CustomerContext _context;
+        public GenericRepository()
+        {
+            _context = new CustomerContext();
+            table = _context.Set<T>();
+        }
+        public GenericRepository(CustomerContext context)
+        {
+            _context = context;
+            table = _context.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+        public T GetById(int id)
+        {
+            return table.Find(id);
+        }
+        public void Add(T obj)
+        {
+            table.Add(obj);
+        }
+        public void Update(T obj)
+        {
+            table.Update(obj);
+        }
+        public void Delete(object id)
+        {
+            T existing = table.Find(id);
+            table.Remove(existing);
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
