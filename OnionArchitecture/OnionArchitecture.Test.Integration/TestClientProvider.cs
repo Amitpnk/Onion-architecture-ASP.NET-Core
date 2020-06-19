4b17acceddf7961e1bf0aaeaac94d57aaf93db4f
Amit P Naik <amit.naik8103@gmail.com>
2020-06-19 14:01:59 +0530

    integration testing

---
+++ OnionArchitecture/OnionArchitecture.Test.Integration/TestClientProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Test.Integration
+{
+    public class TestClientProvider
+    {
+        public HttpClient Client { get; private set; }
+
+        public TestClientProvider()
+        {
+            var server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+
+            Client = server.CreateClient();
+        }
+    }
+}
