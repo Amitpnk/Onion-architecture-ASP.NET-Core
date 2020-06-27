98815576d261a44f524364372ff3e51abe7e11df
Amit P Naik <amit.naik8103@gmail.com>
2020-06-27 09:57:01 +0530

    refactoring

---
+++ OA/OA.Test.Integration/ApiCustomerTest.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using OA.Test.Integration;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace OA.Test.Integration
+{
+    public class ApiCustomerTest
+    {
+        [TestCase("Get", "api/Customer")]
+        [TestCase("Get", "api/Customer/Amazon")]
+        public async Task GetAllCustomerTestAsync(string method, string URL)
+        {
+            using (var client = new TestClientProvider().Client)
+            {
+                var request = new HttpRequestMessage(new HttpMethod(method), URL);
+                var response = await client.SendAsync(request);
+
+                response.EnsureSuccessStatusCode();
+
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+    }
+}
---
+++ OA/OA.Test.Unit/Data/CustomerContextTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using OA.Data;
+using OA.Domain.Entities;
+
+namespace OA.Test.Unit.Data
+{
+    public class CustomerContextTest
+    {
+
+        [Test]
+        public void CanInsertCustomerIntoDatabasee()
+        {
+
+            using (var context = new CustomerContext())
+            {
+                var customer = new Customer();
+                context.Customers.Add(customer);
+                Assert.AreEqual(EntityState.Added, context.Entry(customer).State);
+            }
+        }
+    }
+}
