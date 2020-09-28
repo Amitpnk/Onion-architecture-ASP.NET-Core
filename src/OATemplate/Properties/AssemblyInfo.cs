ae0e04e731b317178f4a0e9f8efed40c23469c59
Amit P Naik <amit.naik8103@gmail.com>
2020-09-28 18:34:28 +0530

    fix: vsix project

--- src/OATemplate/Properties/AssemblyInfo.cs
+++ src/OATemplate/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OA.VsixProjectProject")]
+[assembly: AssemblyTitle("OATemplateProject")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OA.VsixProjectProject")]
+[assembly: AssemblyProduct("OATemplateProject")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
